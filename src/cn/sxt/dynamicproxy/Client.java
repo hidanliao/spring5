62046fe90296c0860e4d5254fb95881e5002519a
hidan <hidantakahashi@gmail.com>
2023-03-27 16:17:36 +0900

    first commit

---
+++ src/cn/sxt/dynamicproxy/Client.java
@@ -0,0 +1,14 @@
+package cn.sxt.dynamicproxy;
+
+import cn.sxt.service.UserService;
+import cn.sxt.service.UserServiceImpl;
+
+public class Client {
+	public static void main(String[] args) {
+		UserService userService = new UserServiceImpl();
+		ProxyInovationHandler pih = new ProxyInovationHandler();
+		pih.setTarget(userService);
+		UserService proxy = (UserService)pih.getProxy();
+		proxy.delete();
+	}
+}
---
+++ src/cn/sxt/dynamicproxy/Host.java
@@ -0,0 +1,7 @@
+package cn.sxt.dynamicproxy;
+
+public class Host implements Rent{
+	public void rent() {
+		System.out.println("家を賃貸しする");
+	}
+}
